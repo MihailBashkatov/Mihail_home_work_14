63b8ca7d7b00a83045db4dcd87987ff901724dc2
home <mihail.bashkatov@gmail.com>
2022-09-16 19:27:22 +0300

    1-st release

---
+++ utils.py
@@ -0,0 +1,126 @@
+# Импорт необходимых библиотек
+import sqlite3
+import json
+
+
+def search_title(user_input):
+    """ Поиск по названию"""
+
+    with sqlite3.connect('netflix.db') as connection:
+        result = connection.cursor()
+        query = """SELECT title, country, release_year, listed_in, description
+                             FROM netflix
+                             WHERE title = ?
+                             ORDER by release_year DESC
+                             LIMIT 1
+                        """
+        result.execute(query, (user_input.title(),))
+        movie = result.fetchall()
+        chosen_movie = {}
+        for item in movie:
+            chosen_movie["title"] = item[0]
+            chosen_movie["country"] = item[1]
+            chosen_movie["release_year"] = item[2]
+            chosen_movie["genre"] = item[3]
+            chosen_movie["description"] = item[4].strip()
+        return chosen_movie
+
+
+def search_by_period(year_one, year_two):
+    """ Поиск по диапазону лет выпуска."""
+
+    with sqlite3.connect('netflix.db') as connection:
+        result = connection.cursor()
+        query = """SELECT title, release_year
+                             FROM netflix
+                             WHERE release_year BETWEEN ? and ?
+                             ORDER by release_year DESC
+                        """
+        result.execute(query, (year_one, year_two))
+        movies = result.fetchall()
+        movies_by_period = []
+        for movie in movies:
+            chosen_movies = dict()
+            chosen_movies["title"] = movie[0]
+            chosen_movies["release_year"] = movie[1]
+            movies_by_period.append(chosen_movies)
+    return movies_by_period
+
+
+def search_by_rating(user_rating):
+    """ Поиск по диапазону рейтингу"""
+
+    pass
+
+
+def search_new_by_genre(genre):
+    """ Поиск по жанру"""
+
+    with sqlite3.connect('netflix.db') as connection:
+        result = connection.cursor()
+        query = """SELECT title, description
+                             FROM netflix
+                             WHERE listed_in = ?
+                             ORDER by release_year DESC
+                             LIMIT 10
+                        """
+        result.execute(query, (genre,))
+        movies = result.fetchall()
+        chosen_movie_list = []
+        for movie in movies:
+            chosen_movie = dict()
+            chosen_movie["title"] = movie[0]
+            chosen_movie["description"] = movie[1].strip()
+            chosen_movie_list.append(chosen_movie)
+        return chosen_movie_list
+
+
+def search_by_actors(actor_one, actor_two):
+    """ Получает в качестве аргумента имена двух актеров,
+           и возвращает список тех, кто играет с ними в паре больше 2 раз."""
+
+    with sqlite3.connect('netflix.db') as connection:
+        result = connection.cursor()
+        query = """SELECT "cast"
+                             FROM netflix
+                             WHERE "cast" LIKE ?
+                             AND "cast" LIKE ?
+                        """
+        result.execute(query, (('%' + actor_one + '%'), ('%' + actor_two + '%')))
+        movies = result.fetchall()
+
+        actors_str = ''
+        for actor in movies:
+            actor = ','.join(actor)
+            actors_str += actor + ', '
+        actors_list = actors_str.split(', ')
+        actors_list_needed = []
+        for act in actors_list:
+            if actors_list.count(act) > 2 and act != actor_one and act != actor_two:
+                if act in actors_list_needed:
+                    continue
+                actors_list_needed.append(act)
+        return actors_list_needed
+
+
+def search_by_types(type_movie, year, genre):
+    """ Получает в качестве аргумента  тип картины (фильм или сериал), год выпуска и ее жанр
+          Возвращает список названий картин с их описаниями в JSON"""
+
+    with sqlite3.connect('netflix.db') as connection:
+        result = connection.cursor()
+        query = """SELECT title, description
+                             FROM netflix
+                             WHERE type = ?
+                              AND release_year = ?
+                              AND listed_in = ?
+                        """
+        result.execute(query, ((type_movie), (year), (genre)))
+        movies = result.fetchall()
+        movie_list = []
+        for movie in movies:
+            movie_dict = dict()
+            movie_dict['title'] = movie[0]
+            movie_dict["description"] = movie[1].strip()
+            movie_list.append(movie_dict)
+        return json.dumps(movie_list)
---
+++ view.py
@@ -0,0 +1,38 @@
+# Импорт библиотеки flask
+from flask import Flask, jsonify
+
+# Импорт необъодимых функций
+from utils import search_title, search_by_period, search_new_by_genre
+
+app = Flask(__name__)
+
+# Распознавание кириллицы
+app.config['JSON_AS_ASCII'] = False
+
+
+@app.route('/movie/<title>', methods=['GET'])
+def get_by_title(title):
+    """ Представление  страницы, с выборкой фильма по названию в формате JSON"""
+
+    movie = search_title(title)
+    return jsonify(movie)
+
+
+@app.route('/movie/<year_one>/to/<year_two>', methods=['GET'])
+def get_by_period(year_one, year_two):
+    """ Представление  страницы, с выборкой фильмов по заданному периоду в формате JSON"""
+
+    movies = search_by_period(year_one, year_two)
+    return jsonify(movies)
+
+
+@app.route('/genre/<genre>', methods=['GET'])
+def get_new_by_genre(genre):
+    """ Представление  страницы, с 10 самых свежих фильмов выбранного жанра в формате JSON"""
+
+    movies = search_new_by_genre(genre)
+    return jsonify(movies)
+
+
+if __name__ == '__main__':
+    app.run(port=10000)
